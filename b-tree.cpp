1f1e787eaecad66283df60289657224aedac7961
kuldeep6445 <55747743+kuldeep6445@users.noreply.github.com>
2020-10-21 10:41:26 +0530

    creating basic framework

--- b-tree.cpp
+++ b-tree.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
+class tree{
+public:
+	int data;
+	tree* left;
+	tree* right;
+	tree(int val){data=val;}
+};
+
 int main(){
-    cout<<"hello world";
-    return 0;
+    
 }
